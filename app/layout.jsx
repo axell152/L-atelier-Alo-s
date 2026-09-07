056a23c145bdd4727a4ea4b2c9c70214e86f588c
axell152 <axell.nicol@outlook.fr>
2026-09-07 14:14:07 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         <header className="sticky top-0 z-50 backdrop-blur-md bg-[#FDFBF7]/90 border-b border-[#EFECE6]">
   <div className="max-w-6xl mx-auto px-4 sm:px-6 h-20 flex items-center justify-between">
     <Link href="/" className="flex items-center gap-3 group">
-  <img src="/logo.png" alt="L'Atelier de l'Aloès" className="h-16 w-auto object-cover rounded-xl shadow-xs" />
+  <img src="/logo.png" alt="L'Atelier de l'Aloès" className="h-25 w-auto object-cover rounded-xl shadow-xs" />
 </Link>
 
     <nav className="flex items-center gap-4 sm:gap-6">
